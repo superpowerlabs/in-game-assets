c13b8a358cf68041280c78bb7327b5a2fc59943f
Francesco Sullo <sullof@gmail.com>
2022-12-08 21:34:31 -0800

    Fix error in the airdrop script

--- scripts/consoleLogAlert.js
+++ scripts/consoleLogAlert.js
@@ -2,8 +2,16 @@ const {execSync} = require("child_process");
 const path = require("path");
 
 function consoleLogAlert() {
-  const result = execSync(`cd ${path.resolve(__dirname, "../contracts")} && grep -r 'import "hardhat' .`).toString();
-  if (/:import/.test(result)) {
+  let error = false;
+  try {
+    const result = execSync(`grep -r 'import "hardhat' ${path.resolve(__dirname, "../contracts")}`).toString();
+    if (/:import/.test(result)) {
+      error = true;
+    }
+  } catch (e) {
+    // nothing found
+  }
+  if (error) {
     throw new Error("At least a console.log has been left in the contracts");
   }
 }
