38ff5747b17fb473e463f1331e080d6369b18fac
Francesco Sullo <sullof@gmail.com>
2022-12-14 13:09:29 -0800

    Fix typo

--- contracts/NftFactory.sol
+++ contracts/NftFactory.sol
@@ -65,7 +65,7 @@ contract NftFactory is UUPSUpgradableTemplate {
   error InconsistentArrays();
   error RepeatedAcceptedToken();
   error InvalidAmountForSale();
-  error OnlyOneTokeForTransactionInPublicSale();
+  error OnlyOneTokenForTransactionInPublicSale();
 
   struct Sale {
     uint16 amountForSale;
@@ -326,7 +326,7 @@ contract NftFactory is UUPSUpgradableTemplate {
       if (_wl.balanceOf(_msgSender(), sales[nftId].whitelistedId) < amount) revert NotEnoughWLSlots();
       tokenAmount = getWlPrice(nftId, paymentToken);
     } else {
-      if (amount > 1) revert OnlyOneTokeForTransactionInPublicSale();
+      if (amount > 1) revert OnlyOneTokenForTransactionInPublicSale();
       tokenAmount = getPrice(nftId, paymentToken);
     }
     proceedsBalances[paymentToken] += tokenAmount;
