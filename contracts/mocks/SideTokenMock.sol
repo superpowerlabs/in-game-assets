cb6d6de3b00b3f662bd4392fd8df90afcc5dbee2
Francesco Sullo <sullof@gmail.com>
2023-02-03 13:01:07 -0800

    Moving external-contracts inside contracts

--- contracts/mocks/SideTokenMock.sol
+++ contracts/mocks/SideTokenMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "../../external-contracts/synr-seed/token/SideToken.sol";
+import "../external-contracts/synr-seed/token/SideToken.sol";
 
 contract SideTokenMock is SideToken {
   function mint(address to, uint256 amount) public override onlyOwner {
